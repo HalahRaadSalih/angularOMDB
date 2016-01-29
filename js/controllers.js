76128a169547860d7eff49ab9225d2932296ce7c
Jess Yang <jessyang650@users.noreply.github.com>
2016-01-28 21:11:10 -0800

    routing working for 2 templates

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,20 @@
-var app = angular.module("OMDB", []);
+var app = angular.module("OMDB", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'partials/header.html',
+			controller: 'Header'
+		}).when('/second', {
+			templateUrl: 'partials/footer.html',
+			controller: 'Footer'
+		});
+});
+
+app.controller('Header', function($scope) {
+	$scope.name = 'This is header controller'
+});
+
+app.controller('Footer', function($scope) {
+	$scope.name = 'This is footer controller'
+});
