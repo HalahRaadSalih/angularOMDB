46e9b2530052ec3d32cf83cb1d54ef87fe48b843
Jess Yang <jessyang650@users.noreply.github.com>
2016-01-30 21:14:41 -0800

    single movie route in progress

--- js/controllers.js
+++ js/controllers.js
@@ -2,15 +2,17 @@ var app = angular.module("OMDB");
 
 app.controller('OMDBController', function($scope, $http, $location) {
 	$scope.submit = function(form) {
-		var movieTitle = $scope.findMovie.replace(' ', '+')
+		var movieTitle = $scope.findMovie.replace(' ', '+');
 		$scope.results = [];
 		// http.get returns a promise, so no need to assign to variable
 		$http.get('http://www.omdbapi.com/?s=' + movieTitle).then(function(data){
-			$scope.results = data.data.Search
-			// $scope.poster = $scope.results[0].Poster
-			console.log($scope.results)
+			$scope.results = data.data.Search;
+			console.log($scope.results);
 		});
-	}
+	};
+	$scope.singleMovieClick = function() {
+		
+	};
 });
 
 // app.controller('MovieController', function($scope, $http) {
